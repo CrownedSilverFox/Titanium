52b31e3b6aa020f4a8f94a30c864cd51764dddee
crownedsilverfox <byskillcfpro@gmail.com>
2016-03-04 20:21:15 +0700

    Отображение вариантов ответов.

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -5,6 +5,7 @@ var green_num = 0;
 var yellow_src = 'http://www.komus.ru/photo/_full/331966_1.jpg';
 var red_src = 'http://static.ofisshop.ru/iblock/089/089ba49b21a547fd17f77bca070517c6.png';
 var TEAM = "red";
+var questData;
 
 $(document).ready(function () {
     load_desk();
@@ -42,17 +43,17 @@ function load_desk() {
         table.append(tr)
     }
 }
-function load_questions() {
+
+function onDataLoad(data){
     var table = $('#questions');
     var onclick = "on_question_click(id)";
-    var json = $.getJSON("data/questions.json");
-    console.log(json);
-    for (var i = 0; i < 4; i++) {
+    questData = data;
+    for (var key in data) {
         var tr = $(document.createElement('tr'));
-        for (var j = 0; j < 4; j++) {
+        for (var i = 0; i < data[key].length; i++) {
             var td = $(document.createElement('td'));
-            var button = $('<button />', {text: 'Вопрос' + (j + 1)});
-            button.attr('id', ('2_' + i) + ('_' + j));
+            var button = $('<button />', {text: key + ': ' + data[key][i].cost});
+            button.attr('id', "" + data[key][i].id);
             td.append(button);
             tr.append(td);
             button.attr('onclick', onclick)
@@ -61,13 +62,26 @@ function load_questions() {
     }
 }
 
+function load_questions() {
+    $.getJSON("data/questions.json", onDataLoad)
+}
+
 function on_question_click(id) {
-    var student = {name: "Vasya"};
-    $.post("http://localhost:8000/test", JSON.stringify(student),
-        function (data, status) {
-            console.log(data);
-            console.log(status);
-        });
+    var answBlock = $("#answer_choice");
+    answBlock.empty();
+    for (var key in questData) {
+        for (var i = 0; i < questData[key].length; i++) {
+            if (id == questData[key][i].id) {
+                for (var j = 0; j < (questData[key][i]["answers"]).length; j++) {
+                    var line = '<input type="radio" id="'+ j + '' + questData[key][i].id +'"><label>'
+                        + questData[key][i]["answers"][j] +'</label><br>';
+                    answBlock.append(line);
+                    //var radio = $('<input />', {type: 'radio', id: 'q' + id})
+
+                }
+            }
+        }
+    }
 }
 
 function on_table_click(id) {
